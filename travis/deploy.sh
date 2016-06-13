ef12421cda7fd4aed8df412ace44abb51f2b5285
Andrew Melvin <amelvin@Andrew-MBA.local>
2016-06-13 23:48:50 +0100

    fix port

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -9,14 +9,10 @@ python $TRAVIS_BUILD_DIR/travis/buildmanifest.py /tmp/package /tmp/package/manif
 
 #  no host checking
 #ssh -v -p 4022 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /tmp/travis.key $HOME_USER@$HOME_IP "mkdir -p ~/projects/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/latest/"
-#scp -v -P 4022 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /tmp/travis.key -rp /tmp/package/. "$HOME_USER@$HOME_IP:~/projects/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/latest/"  
+#scp -v -P 4022 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /tmp/travis.key -rp /tmp/package/. "$HOME_USER@$HOME_IP:~/projects/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/latest/"
 #echo "repo slug = $TRAVIS_REPO_SLUG"
 
 echo "DEPLOY HOME"
- 
-ssh -v -p 4022  -i /tmp/travis.key $HOME_USER@$HOME_IP "mkdir -p ~/projects/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/$1/"
-scp -v -P 4022  -i /tmp/travis.key -rp /tmp/package/. "$HOME_USER@$HOME_IP:~/projects/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/$1/"  
 
-
-
-
+ssh -v -p 4022  -i /tmp/travis.key $HOME_USER@www.amelvin.co.uk "mkdir -p ~/projects/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/$1/"
+scp -v -P 4022  -i /tmp/travis.key -rp /tmp/package/. "$HOME_USER@$www.amelvin.co.uk:~/projects/$TRAVIS_REPO_SLUG/$TRAVIS_BRANCH/$1/"  
