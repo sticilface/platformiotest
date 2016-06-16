db04ce22e37ae2b6063caa96a81b6791ab37ba9d
sticilface <amelvin@gmail.com>
2016-06-16 21:09:39 +0100

    test

--- testfolder/testfolder.ino
+++ testfolder/testfolder.ino
@@ -1,67 +1,91 @@
 /*
- *  This sketch sends a message to a TCP server
+ *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
+ *
+ *  You need to get streamId and privateKey at data.sparkfun.com and paste them
+ *  below. Or just customize this script to talk to other HTTP servers.
  *
  */
 
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
 
-ESP8266WiFiMulti WiFiMulti;
+const char* ssid     = "your-ssid";
+const char* password = "your-password";
+
+const char* host = "data.sparkfun.com";
+const char* streamId   = "....................";
+const char* privateKey = "....................";
 
 void setup() {
-    Serial.begin(115200);
-    delay(10);
+  Serial.begin(115200);
+  delay(10);
 
-    // We start by connecting to a WiFi network
-    WiFiMulti.addAP("SSID", "passpasspass");
+  // We start by connecting to a WiFi network
 
-    Serial.println();
-    Serial.println();
-    Serial.print("Wait for WiFi... ");
-
-    while(WiFiMulti.run() != WL_CONNECTED) {
-        Serial.print(".");
-        delay(500);
-    }
-
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
+int value = 0;
 
 void loop() {
-    const uint16_t port = 80;
-    const char * host = "192.168.1.1"; // ip or dns
+  delay(5000);
+  ++value;
 
-
-
-    Serial.print("connecting to ");
-    Serial.println(host);
-
-    // Use WiFiClient class to create TCP connections
-    WiFiClient client;
-
-    if (!client.connect(host, port)) {
-        Serial.println("connection failed");
-        Serial.println("wait 5 sec...");
-        delay(5000);
-        return;
+  Serial.print("connecting to ");
+  Serial.println(host);
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  
+  // We now create a URI for the request
+  String url = "/input/";
+  url += streamId;
+  url += "?private_key=";
+  url += privateKey;
+  url += "&value=";
+  url += value;
+  
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
     }
-
-    // This will send the request to the server
-    client.print("Send this data to server");
-
-    //read back one line from server
+  }
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
     String line = client.readStringUntil('\r');
-    client.println(line);
-
-    Serial.println("closing connection");
-    client.stop();
-
-    Serial.println("wait 5 sec...");
-    delay(5000);
+    Serial.print(line);
+  }
+  
+  Serial.println();
+  Serial.println("closing connection");
 }
